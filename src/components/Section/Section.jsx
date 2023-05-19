d8913bca5cc0f8a0d43e615feb0d84ec7859514b
Andrii Veremii <dashuk10@gmail.com>
2023-05-19 21:55:42 +0300

    add

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container } from './Section.styled';
+
+
+import Header from 'components/Header/Header';
+
+const Section = ({ title, children }) => {
+  return (
+    <Container>
+      <Header title={title} />
+      {children}
+    </Container>
+  );
+};
+
+export default Section;
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.any.isRequired,
+};
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.section`
+  background-color: #fff;
+  border: 1px solid #2196f3;
+  display: flex;
+  margin: 0 auto;
+  flex-direction: column;
+  align-items: center;
+  max-width: 100%;
+  width: 600px;
+  box-shadow: -1px -1px 15px #2196f3, 15px 15px 35px #fff;
+`;
